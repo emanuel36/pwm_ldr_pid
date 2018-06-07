765d9c8ce6bc63458ade16681e8363e0f92d5679
emanuel36 <emanuel_ec@alu.ufc.br>
2018-06-07 19:45:31 -0300

    potenciometro

--- src/main.c
+++ src/main.c
@@ -2,19 +2,30 @@
 #include <unistd.h>
 #include "embedded_linux.h"
 
-#define PERIOD 10000
+#define PERIOD 			2500
+#define MIN_DUTY		0
+#define MAX_DUTY		2500
+#define LDR_SENSOR 		0
+#define POTENCIOMETRO	2
+#define MAX_SET_POINT 	1500
+
 
 int main(){
 	init_analog_pins();
 	config_pwm();
 	set_pwm_period(PERIOD);
-	set_pwm_duty(1);
+	set_pwm_duty(MIN_DUTY);
 	init_pwm();
-	int leitura, duty = 0, set_point = 1500;
+	int leitura, duty = MIN_DUTY, set_point;
 	double pwm;
 
 	while(1){
-		leitura = get_value_ain(0);
+		set_point = get_value_ain(POTENCIOMETRO);
+		leitura = get_value_ain(LDR_SENSOR);
+
+		if(set_point > MAX_SET_POINT){
+			set_point = MAX_SET_POINT;
+		}
 
 		if(leitura < set_point){
 			duty++;
@@ -22,11 +33,13 @@ int main(){
 		else if(leitura > set_point){
 			duty--;
 		}
-		if(duty < 0 | duty > 10000){
-			duty = 1;
+
+		if(duty < MIN_DUTY | duty > MAX_DUTY){
+			duty = MIN_DUTY;
 		}
+
 		set_pwm_duty(duty);
 		pwm = ((double) duty/ (double) PERIOD) * 100;
-		printf("Sensor: %d | PWM: %.2lf%\n", leitura, pwm);
+		printf("Set Point: %d | Sensor: %d | PWM: %.2lf%\n", set_point, leitura, pwm);
 	}
 }
