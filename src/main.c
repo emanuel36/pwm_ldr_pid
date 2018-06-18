6d3907aa120bf17b6945fdc1e8f612976891a125
emanuel36 <emanuel_ec@alu.ufc.br>
2018-06-18 11:16:18 -0300

    Controlador PID

--- src/main.c
+++ src/main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include "embedded_linux.h"
 
 #define PERIOD 			2500
 #define MIN_DUTY		0
-#define MAX_DUTY		2500
 #define LDR_SENSOR 		0
 #define POTENCIOMETRO	2
 #define MAX_SET_POINT 	1500
 
-int ajusta_duty(int duty, int set_point, int leitura);
+double error_meas,
+	   kp = 1.0,
+	   ki = 0.0,
+	   kd = 0.0,
+	   proportional = 0,
+	   integral = 0,
+	   derivative = 0,
+	   PID,
+	   tempo;
+
+int measure,
+    lastMeasure,
+    duty = 0,
+    set_point;
+
 int verifica_set_point(int duty);
+void pid_control();
 
 int main(){
 	init_analog_pins();
@@ -18,38 +33,31 @@ int main(){
 	set_pwm_period(PERIOD);
 	set_pwm_duty(MIN_DUTY);
 	init_pwm();
-	int leitura, duty = MIN_DUTY, set_point;
+	int leitura;
 	double pwm;
 
+	clock_t Ticks[2];
+    Ticks[0] = clock();
+
 	while(1){
+		Ticks[1] = clock();
+		tempo = (Ticks[1] - Ticks[0]) * 1000 / CLOCKS_PER_SEC;
+		Ticks[0] = clock();	
+
 		set_point = get_value_ain(POTENCIOMETRO);
 
-		leitura = get_value_ain(LDR_SENSOR);
+		measure = get_value_ain(LDR_SENSOR);
 
 		set_point = verifica_set_point(set_point);
 
-		duty = ajusta_duty(duty, set_point, leitura);
+		pid_control();
 
 		set_pwm_duty(duty);
 
 		pwm = ((double) duty/ (double) PERIOD) * 100;
 
-		printf("Set Point: %d | Sensor: %d | PWM: %.2lf%\n", set_point, leitura, pwm);
+		printf("Set Point: %d | Sensor: %d | PWM: %.2lf%\n", set_point, measure, pwm);
 	}
-}
-
-int ajusta_duty(int duty, int set_point, int leitura){
-	if(leitura < set_point){
-			duty++;
-	}
-	else if(leitura > set_point){
-			duty--;
-	}
-
-	if(duty < MIN_DUTY | duty > MAX_DUTY){
-		return MIN_DUTY;
-	}
-	return duty;
 }
 
 int verifica_set_point(int set_point){
@@ -57,4 +65,28 @@ int verifica_set_point(int set_point){
 		return MAX_SET_POINT;
 	}
 	return set_point;
+}
+
+void pid_control(){
+    error_meas = (double) set_point - measure;
+
+    proportional = error_meas * kp;
+
+    integral += error_meas * ki * tempo;
+
+    derivative = (lastMeasure - measure) * kd * tempo; 
+
+    lastMeasure = measure;
+
+    PID = proportional + integral + derivative;
+    
+    duty = PID + duty;
+
+    if(duty > PERIOD){
+    	duty = PERIOD;	
+    } 
+
+    if(duty < 0){
+    	duty = 0;
+    }
 }
